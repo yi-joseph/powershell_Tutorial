6cbb94cc7547d6bda6f42c6c6ec4e8814f00d1c9
yi-joseph <yimiaoheidelberg@gmail.com>
2025-07-17 16:12:37 +0200

    Update Grundlagen_powershell.ps1

--- Grundlagen_powershell.ps1
+++ Grundlagen_powershell.ps1
@@ -2,3 +2,23 @@
 $PSVersionTable
 # Man kann auch die Objekte von Version Table weiterhin anzeigen lassen: 
 $PSVersionTable.PSVersion
+
+# 2 aktuelles Datum anzeigen
+get-date
+
+# Einen String in Konsole anzeigen
+Write-Host "Hallo Yi Miao in Powershell-Welt! "
+
+# Eine Variable Name deklarieren und initialisieren 
+# Dann in einem String eingeben und ausgeben lassen
+# Hier ist ein Programm von Hallo-Welt von Powershell
+$Name = "Yi Miao"
+Write-Host "Hallo $Name, hier ist die Welt von Powershell! `n" 
+
+# Zeilenumbrech: `n
+
+# Aktuelles Datum anzeigen
+$Datum = Get-Date
+Write-Host "Heute ist $Datum `n"
+
+
